c802d2cbf3505ba56c64bff2111da151b7dc133d
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-14 16:18:25 -0400

    Completed the code for the d and c algo

--- divide-and-conquer/alg3.py
+++ divide-and-conquer/alg3.py
@@ -12,12 +12,50 @@
 from sys import argv
 
 def enumer_d_and_c(values):
-    pass
+    # Checking 
+    if len(values) == 0:
+        return 0, values
+    elif len(values) == 1:
+        return values[0], values
 
+    tempmax = 0
+    midmax = 0
+    midstart = 0
+    midend = 0
 
+    leftmax = 0
+    rightmax = 0
 
+    midpoint = int(len(values) / 2)
 
+    midstart = midpoint
+    midend = midpoint
 
+    for i in reversed(range(midpoint)):
+        tempmax += values[i]
+        if tempmax > leftmax:
+            leftmax = tempmax
+            midstart = i
+
+    tempmax = 0
+
+    for i in range(midpoint, len(values)):
+        tempmax += values[i]
+        if tempmax > rightmax:
+            rightmax = tempmax
+            midend = i + 1
+
+    midmax = leftmax + rightmax
+
+    leftmax, leftsubarr = enumer_d_and_c(values[:midpoint])
+    rightmax, rightsubarr = enumer_d_and_c(values[midpoint:])
+
+    if midmax >= leftmax and midmax >= rightmax:
+        return midmax, values[midstart:midend]
+    elif leftmax >= rightmax and leftmax > midmax:
+        return leftmax, leftsubarr
+    elif rightmax > leftmax and rightmax > midmax:
+        return rightmax, rightsubarr
 
 
 if __name__ == "__main__":
