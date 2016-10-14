2c546490efdb51212f91f6eb9bb7b68d64177449
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-14 10:36:42 -0400

    Added logic for using alg3 as a cl utility

--- divide-and-conquer/alg3.py
+++ divide-and-conquer/alg3.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 #--------------------------------------------------------------------
 # Function: enumer_d_and_c
 # Description: Computes the maximum sub-array and the associated sum using a div#    ide and conquer algorithm
@@ -6,5 +8,17 @@
 # Preconditions: "values constains at least one positive integer
 #--------------------------------------------------------------------
 
+# Importing argv to allow the method to be used as a CL utility
+from sys import argv
+
 def enumer_d_and_c(values):
-    
+    pass
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(enumer_d_and_c([int(x) for x in argv[1:]]))
