dfb01e94f846db044eadfd1a622d56eff71a992e
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-14 10:23:29 -0400

    Added comment header to alg3

---
+++ divide-and-conquer/alg3.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------
+# Function: enumer_d_and_c
+# Description: Computes the maximum sub-array and the associated sum using a div#    ide and conquer algorithm
+# Receives: values - list of integers
+# Returns: maximum sub-array sum, and maximum sub-array
+# Preconditions: "values constains at least one positive integer
+#--------------------------------------------------------------------
+
+def enumer_d_and_c(values):
+    
