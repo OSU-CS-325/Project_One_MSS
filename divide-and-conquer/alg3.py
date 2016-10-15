db54fcf19101204a9aa408da9d230277d2fe5fae
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-14 17:52:56 -0700

    Committing old divide and conquer

---
+++ divide-and-conquer/alg3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#------------------------------------------------------------------
+# Function: divide_and_conquor
+# Description: Computes the maximum sub-array and the associated sum using a div# ide and conquor enumeration algorithm
+# Receives: values - list/array of integers
+# Returns: maximum sub-array sum, and maximum sub-array
+# Preconditions: "values" contains at least one positive integer
+#-----------------------------------------------------------------
+
+from sys import argv
+
+def divide_and_conquor(values):
+  
